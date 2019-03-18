971a2df34873fc6ba172cbea7d9b388c8481b1c4
Panda L <jbvhl@hotmail.com>
2019-03-18 15:39:17 -0600

    1539

--- db/diagnose/getSymp.sql
+++ db/diagnose/getSymp.sql
@@ -1,3 +1,5 @@
 select *
 from symptom
+join patient
+on patient.id = symptom.patient_id
 where patient_id = ${id}
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -3,25 +3,47 @@ import "./landing.css";
 import axios from "axios";
 
 class Landing extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      articles: []
+    }
+  }
+
   componentDidMount() {
     this.getArticles();
   }
 
   getArticles = async () => {
     await axios.get(`https://newsapi.org/v2/top-headlines?category=health&apiKey=${process.env.REACT_APP_apiKey}`).then(res => {
-      return res.data
+      this.setState({
+        articles: res.data.articles
+      });
     })
   }
 
 
 
   render() {
+    const articles = this.state.articles.slice(0, 3).map((article, i) => {
+        return (
+          <div key={i}>
+            <img src={article.urlToImage}/>
+            <h3>
+              {article.title}
+            </h3>
+            <p>{article.description}</p>
+          </div>
+        )
+    })
+
     return (
       <div>
         <h1>Diagnosed</h1>
         <div>
           Articles
-          {this.getArticles}
+          {articles}
         </div>
       </div>
     );
